d4d45b18258f70453fef9ba74081da3b2aa90a1e
Andrew Lin <andrew.lin@ieee.org>
2020-01-23 18:58:41 -0800

    Network port as command line argument.

--- buzzword_bingo.py
+++ buzzword_bingo.py
@@ -14,6 +14,12 @@ def parse_command_line() -> argparse.Namespace:
     """Get command line arguments."""
     parser = argparse.ArgumentParser()
     parser.add_argument('buzzwords', help='path to buzzword file.')
+    parser.add_argument(
+        '-p', '--port',
+        type=int,
+        default=5000,
+        help='network port number'
+    )
 
     args = parser.parse_args()
     args.buzzwords = os.path.abspath(os.path.expanduser(args.buzzwords))
@@ -24,7 +30,7 @@ def main() -> None:
     """Launch application."""
     args = parse_command_line()
     bingo_server.app.set_buzzwords(args.buzzwords)
-    bingo_server.app.run(debug=False, host="0.0.0.0")
+    bingo_server.app.run(debug=False, host='0.0.0.0', port=args.port)
 
 
 if __name__ == '__main__':
